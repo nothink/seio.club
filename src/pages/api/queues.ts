c35faf8dca4dc3aa579585da11f2e6422989b672
nothink <gakongakon@gmail.com>
2022-09-03 22:56:01 +0900

    fix: create test api

--- src/pages/api/queues.ts
+++ src/pages/api/queues.ts
@@ -9,5 +9,12 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if (req.method !== "POST") {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+  if (req.body instanceof Array) {
+    console.log(req.body);
+  }
   res.status(200).json({ name: "John Doe" });
 }
