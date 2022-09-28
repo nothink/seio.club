6fa5894d9697e36f2396603ce55b922d9dcbfc75
nothink <gakongakon@gmail.com>
2022-09-29 01:26:16 +0900

    fix: re-insall cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions
 // and set up triggers.
 const axios = require("axios").default;
+const cors = require("cors")({ origin: true });
 const functions = require("firebase-functions");
 
 // The Firebase Admin SDK to access Firestore.
@@ -14,23 +15,24 @@ const REGION = "asia-northeast1";
 
 // Take the body json passed to this HTTP endpoint and insert it into
 // Firestore under the path /dqx9mbrpz1jhx/:documentId/urls
-exports.dqx9mbrpz1jhx = functions
-  .region(REGION)
-  .https.onRequest(async (req, res) => {
-    res.set("Access-Control-Allow-Origin", "*");
+exports.dqx9mbrpz1jhx = functions.region(REGION).https.onRequest((req, res) => {
+  cors(req, res, () => {
     if (req.method === "POST") {
       const urls = req.body.urls;
       // Push the new message into Firestore using the Firebase Admin SDK.
-      const result = await admin
+      const result = admin
         .firestore()
         .collection(RES_COL)
-        .add({ urls: urls });
-      functions.logger.log("added dqx9mbrpz1jhx ID: ", result.id);
+        .add({ urls: urls })
+        .then((result) => {
+          functions.logger.log("added dqx9mbrpz1jhx ID: ", result.id);
+        });
       res.send(201);
     } else {
       res.send(405);
     }
   });
+});
 
 // Listens for new messages added to /dqx9mbrpz1jhx/:documentId
 exports.fetch = functions
